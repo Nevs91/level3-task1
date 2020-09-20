27ea5cbaca9fa9595596f046066358b41ba8fb06
Sven van Putten <svenvanputten@gmail.com>
2020-09-20 21:58:01 +0200

    Added actions to the nav_graph and created a click event to show the rating fragment

--- app/src/main/java/com/example/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -1,10 +1,12 @@
 package com.example.madlevel3task1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -21,5 +23,11 @@ class StartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnStartRating.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_StartFragment_to_RatingFragment
+            )
+        }
     }
 }

9b490e6cd1a4aaddb95f675c9d87a3fe84137370
Sven van Putten <svenvanputten@gmail.com>
2020-09-21 10:21:24 +0200

    Recreated a nav_graph action, re-added the onCreateView method for the SummaryFragment

--- app/src/main/java/com/example/madlevel3task1/SummaryFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/SummaryFragment.kt
@@ -1,7 +1,9 @@
 package com.example.madlevel3task1
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_summary.*
@@ -10,6 +12,14 @@ const val ARG_GAME_NAME = "arg_game_name"
 const val ARG_GAME_RATING = "arg_game_rating"
 
 class SummaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
